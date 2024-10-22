9ef09b9c48bc77a56ac496f7b8086e23d2d5869e
nulmer-student <nulmer@ualberta.ca>
2024-10-21 21:42:03 -0600

    Add basic admin class

---
+++ Espresso/app/src/main/java/com/example/espresso/Admin.java
@@ -0,0 +1,14 @@
+package com.example.espresso;
+
+import android.content.Context;
+
+public class Admin extends User {
+    /**
+     * Create a new admin user.
+     *
+     * @param context App context.
+     */
+    Admin(Context context) {
+        super(context);
+    }
+}
