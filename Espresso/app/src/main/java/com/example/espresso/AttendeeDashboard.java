1a57ea6a381676c8cc84465bf5dfa25b490c136b
EdDee296 <109675085+EdDee296@users.noreply.github.com>
2024-11-05 12:36:16 -0700

    finish fetching user data and display in profile tab

--- Espresso/app/src/main/java/com/example/espresso/AttendeeDashboard.java
+++ Espresso/app/src/main/java/com/example/espresso/AttendeeDashboard.java
@@ -1,12 +1,19 @@
 package com.example.espresso;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.espresso.databinding.ActivityMainBinding;
+import com.example.espresso.databinding.EntrantHomeBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.lang.reflect.Array;
@@ -15,16 +22,42 @@ import java.util.Collections;
 import java.util.List;
 
 public class AttendeeDashboard extends AppCompatActivity {
-
+    EntrantHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.entrant_home);
-        // fetch  list of events from firebase
+        binding = EntrantHomeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // Navigation
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.events) {
+                    // Open events activity
+                    Log.d("BottomNav", "Events clicked");
+            }
+            else if (item.getItemId() == R.id.scan) {
+                    // Open scan activity
+                    Log.d("BottomNav", "Scan clicked");
+            }
+            else if (item.getItemId() == R.id.profile) {
+                // Open profile activity
+                Log.d("BottomNav", "Profile clicked");
+                Intent intent = new Intent(AttendeeDashboard.this, AttendeeProfile.class);
+                startActivity(intent);
+            }
+            return true;
+        });
+
         ListView listView = findViewById(R.id.event_list_view);
         EventAdapter adapter = new EventAdapter(this, getEventsName(), getEventsDate(), getEventsTime(), getEventsLocation(), getEventsImage());
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Open event details activity
+            }
+        });
     }
 
     private String[] getEventsName() {
@@ -36,18 +69,19 @@ public class AttendeeDashboard extends AppCompatActivity {
         String[] events = {"nov 10th, 2023", "dec 10th, 2023", "feb 10th, 2023"};
         return events;
     }
+
     private String[] getEventsTime() {
         String[] events = {"10:00 AM", "9:00 AM", "3:00 PM"};
         return events;
     }
+
     private String[] getEventsLocation() {
         String[] events = {"Los Angeles", "New York", "Toronto"};
         return events;
     }
+
     private String[] getEventsImage() {
         String[] events = {"https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww", "https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww", "https://plus.unsplash.com/premium_photo-1683865776032-07bf70b0add1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXJsfGVufDB8fDB8fHww"};
         return events;
     }
-
-
 }
---
+++ Espresso/app/src/main/java/com/example/espresso/AttendeeProfile.java
@@ -0,0 +1,94 @@
+package com.example.espresso;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.espresso.databinding.EntrantHomeBinding;
+import com.example.espresso.databinding.EntrantProfileBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
+
+public class AttendeeProfile extends AppCompatActivity {
+    EntrantProfileBinding binding;
+    String deviceID, name, email, phone, type;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = EntrantProfileBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        deviceID = new User(this).getDeviceID();
+        if (deviceID == null) {
+            Log.d("user", "Device ID is null");
+            return;
+        }
+        // Navigation
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.events) {
+                // Open events activity
+                Log.d("BottomNav", "Events clicked");
+            }
+            else if (item.getItemId() == R.id.scan) {
+                // Open scan activity
+                Log.d("BottomNav", "Scan clicked");
+            }
+            else if (item.getItemId() == R.id.home) {
+                // Open profile activity
+                Log.d("BottomNav", "Home clicked");
+                Intent intent = new Intent(AttendeeProfile.this, AttendeeDashboard.class);
+                startActivity(intent);
+            }
+            return true;
+        });
+
+        //Fetching data from Firebase
+        DocumentReference docRef = db.collection("users").document(deviceID);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("user", "DocumentSnapshot data: " + document.getData());
+                        Map<String, Object> data = document.getData();
+                        name = data.get("name").toString();
+                        email = data.get("email").toString();
+                        phone = data.get("phone").toString();
+                        type = data.get("type").toString();
+
+                        //Setting data to textviews
+                        binding.name.setText(name);
+                        binding.email.setText(String.format("Email: %s", email));
+                        binding.phoneNumber.setText(String.format("Phone Number: %s", phone));
+                        binding.role.setText(String.format("Role: %s", type));
+
+                    } else {
+                        Log.d("user", "No such document");
+                    }
+                } else {
+                    Log.d("user", "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+}
