e7d7da6979b1e7081fb87d6ae2546073fac61a2e
EdDee296 <109675085+EdDee296@users.noreply.github.com>
2024-11-05 12:36:16 -0700

    finish fetching user data and display in profile tab

--- Espresso/app/src/main/java/com/example/espresso/AttendeeProfile.java
+++ Espresso/app/src/main/java/com/example/espresso/AttendeeProfile.java
@@ -1,32 +1,39 @@
 package com.example.espresso;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.espresso.databinding.AttendeeProfileBinding;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.espresso.databinding.EntrantHomeBinding;
+import com.example.espresso.databinding.EntrantProfileBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.Map;
-import java.util.Objects;
+
 
 public class AttendeeProfile extends AppCompatActivity {
-    AttendeeProfileBinding binding;
+    EntrantProfileBinding binding;
     String deviceID, name, email, phone, type;
-    Button logout;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = AttendeeProfileBinding.inflate(getLayoutInflater());
+        binding = EntrantProfileBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
         deviceID = new User(this).getDeviceID();
@@ -34,19 +41,6 @@ public class AttendeeProfile extends AppCompatActivity {
             Log.d("user", "Device ID is null");
             return;
         }
-        logout = findViewById(R.id.button);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if (user != null) {
-                    FirebaseAuth.getInstance().signOut();
-                    Log.d("user", "User signed out");
-                }
-                Intent intent = new Intent(AttendeeProfile.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
         // Navigation
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.events) {
@@ -60,11 +54,12 @@ public class AttendeeProfile extends AppCompatActivity {
             else if (item.getItemId() == R.id.home) {
                 // Open profile activity
                 Log.d("BottomNav", "Home clicked");
-                Intent intent = new Intent(AttendeeProfile.this, AttendeeHomeActivity.class);
+                Intent intent = new Intent(AttendeeProfile.this, AttendeeDashboard.class);
                 startActivity(intent);
             }
             return true;
         });
+
         //Fetching data from Firebase
         DocumentReference docRef = db.collection("users").document(deviceID);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -75,20 +70,17 @@ public class AttendeeProfile extends AppCompatActivity {
                     if (document.exists()) {
                         Log.d("user", "DocumentSnapshot data: " + document.getData());
                         Map<String, Object> data = document.getData();
+                        name = data.get("name").toString();
+                        email = data.get("email").toString();
+                        phone = data.get("phone").toString();
+                        type = data.get("type").toString();
 
-                        assert data != null;
-
-                        name = Objects.requireNonNull(data.get("name")).toString();
-                        email = Objects.requireNonNull(data.get("email")).toString();
-                        phone = Objects.requireNonNull(data.get("phone")).toString();
-                        type = Objects.requireNonNull(data.get("type")).toString();
-
-                        // Setting data to TextView
-
+                        //Setting data to textviews
                         binding.name.setText(name);
                         binding.email.setText(String.format("Email: %s", email));
                         binding.phoneNumber.setText(String.format("Phone Number: %s", phone));
                         binding.role.setText(String.format("Role: %s", type));
+
                     } else {
                         Log.d("user", "No such document");
                     }
@@ -98,4 +90,5 @@ public class AttendeeProfile extends AppCompatActivity {
             }
         });
     }
+
 }
