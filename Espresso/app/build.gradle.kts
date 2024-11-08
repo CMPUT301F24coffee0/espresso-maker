b0165ebf62c2d5d6d501942307bc6108e10371dc
EdDee296 <109675085+EdDee296@users.noreply.github.com>
2024-11-07 21:08:20 -0700

    finish generate qr for events

--- Espresso/app/build.gradle.kts
+++ Espresso/app/build.gradle.kts
@@ -66,5 +66,8 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
     implementation ("com.squareup.picasso:picasso:2.8")
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
+    implementation ("com.google.zxing:core:3.4.1")
+
 
 }
