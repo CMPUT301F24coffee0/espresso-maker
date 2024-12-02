9b5582e403e654b2ca43690eb7206c96ff6398ca
nulmer-student <nulmer@ualberta.ca>
2024-12-02 06:12:15 -0700

    Add poster test

---
+++ Espresso/app/src/androidTest/java/com/example/espresso/db/PosterTest.java
@@ -0,0 +1,62 @@
+package com.example.espresso.db;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import android.app.Activity;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.espresso.MainActivity;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Test the image upload.
+ */
+@RunWith(AndroidJUnit4.class)
+public class PosterTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    private InputStream loadImage(Activity activity, String path) {
+        try {
+            return activity.getAssets().open(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Upload an poster to the database.
+     */
+    @Test
+    public void uploadPoster() {
+        rule.getScenario().onActivity(activity -> {
+            InputStream img = loadImage(activity,"user-list-svgrepo-com.svg");
+
+            // Where to upload
+            String eventID = "1234";
+            StorageReference storage = FirebaseStorage.getInstance().getReference();
+            StorageReference imgRef = storage.child("posters/" + eventID + ".svg");
+
+            // Upload the image
+            assertNotNull(img);
+            UploadTask task = imgRef.putStream(img);
+            task.addOnFailureListener(taskSnapshot -> {
+                // Error out if the image failed to upload
+                fail();
+            });
+        });
+    }
+}
